02f5c7d8e86ede03947727cf263cf1041ed487e7
entakar <takaendo007@gmail.com>
2020-04-16 00:32:14 +0900

    CREATE 2020/04/15 PAINTTIMER

---
+++ PAINTTIMER/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PAINTTIMER
+//
+//  Created by EndoTakashi on 2020/04/16.
+//  Copyright © 2020 tak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
